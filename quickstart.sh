ac8d994f6fbb5b7fdb99fb647cc648f465e4880f
jim bromfield <jbromfield@macbook.lan>
2025-10-12 15:44:23 -0400

    update env file

--- quickstart.sh
+++ quickstart.sh
@@ -1,11 +1,2 @@
-
-make setup        # Installs everything, starts ES + Ollama
-
-# Start services
-make start        # API + UI
-
-# Or use Docker
 docker-compose up -d
-
-# Access UI
 open http://localhost:8501
